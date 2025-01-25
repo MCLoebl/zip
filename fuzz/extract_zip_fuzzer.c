b37f6f65868be5c61dab96172ea20c6106549ae6
MCLoebl <matthias.loebl@rwth-aachen.de>
2025-01-25 17:48:07 +0100

    new fuzzer

--- fuzz/extract_zip_fuzzer.c
+++ fuzz/extract_zip_fuzzer.c
@@ -13,11 +13,9 @@ int on_extract_entry(const char *filename, void *arg) {
 int LLVMFuzzerTestOneInput(const uint8_t *data, const size_t size)
 {
     char* temp_name = tmpnam(NULL);
-    struct zip_t *zip = zip_open(temp_name, ZIP_DEFAULT_COMPRESSION_LEVEL, 'w');
-    zip_entry_open(zip, "test_file.txt");
-    zip_entry_write(zip, data, size);
-    zip_entry_close(zip);
-    zip_close(zip);
+    FILE* file = fopen(temp_name, "wb");
+    fwrite(data, size, 1, file);
+    fclose(file);
 
     int arg = 2;
     zip_extract(temp_name, "/tmp", on_extract_entry, &arg);
