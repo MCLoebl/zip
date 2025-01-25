997cfd52aad84ba2b9242ca0d00757f06df869a9
MCLoebl <matthias.loebl@rwth-aachen.de>
2025-01-25 19:20:06 +0100

    correct

--- fuzz/stream_extract_zip_fuzzer.c
+++ fuzz/stream_extract_zip_fuzzer.c
@@ -11,13 +11,7 @@ int on_extract_entry(const char *filename, void *arg) {
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, const size_t size)
 {
-    size_t zipstreamsize;
-    struct zip_t *zip = zip_stream_open(NULL, zipstreamsize, ZIP_DEFAULT_COMPRESSION_LEVEL, 'w');
-    zip_entry_open(zip, "test");
-    zip_entry_write(zip, data, size);
-    zip_entry_close(zip);
-    int arg = 2;
-    zip_stream_extract(zip, zipstreamsize, "/tmp", on_extract_entry, &arg);
+    zip_stream_extract(data, size, "/tmp", on_extract_entry, &arg);
     zip_stream_close(zip);
 
     return 0;
